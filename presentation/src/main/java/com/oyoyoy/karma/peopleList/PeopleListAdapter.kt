1c69a47dee15405aea4d6eda21bb8f7b26d48a53
joyeum-smoothy <doyeon.eum@smoothy.co>
2021-07-19 16:20:31 +0900

    RecyclerView 드디어 구현 - databinding 사용하지 않음 - BaseFragrment 말고 Fragment 상속 - 새로운 데이터 시 업데이트 하는건 x - list는 viewmodel, 다른 생명주기는 fragment에서

--- presentation/src/main/java/com/oyoyoy/karma/peopleList/PeopleListAdapter.kt
+++ presentation/src/main/java/com/oyoyoy/karma/peopleList/PeopleListAdapter.kt
@@ -2,36 +2,59 @@ package com.oyoyoy.karma.peopleList
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.LiveData
+import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.oyoyoy.karma.R
 import com.oyoyoy.karma.peopleList.test.Person
 
-class PeopleListAdapter(var people: LiveData<List<Person>>): RecyclerView.Adapter<PeopleListAdapter.PersonHolder>() {
-    inner class PersonHolder constructor(parent: ViewGroup)
-        : RecyclerView.ViewHolder(LayoutInflater.from(parent.context)
-        .inflate(R.layout.item_people_list, parent, false)) {
-        var name = itemView.textF
-        var content = itemView.textS
+
+class PeopleListAdapter(var people: MutableLiveData<List<Person>>): RecyclerView.Adapter<PeopleListAdapter.PersonHolder>() {
+    inner class PersonHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+        var textViewName : TextView = itemView.findViewById(R.id.name)
+        var textViewContent : TextView = itemView.findViewById(R.id.content)
+        var textViewTime : TextView = itemView.findViewById(R.id.time)
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PeopleListAdapter.PersonHolder {
-        return PersonHolder(parent)
+
+        var temp1 = Person("one","test1",11111)
+        var temp2 = Person("two","test2",22222)
+        var temp3 = Person("three","test3",33333)
+
+        people.value = listOf(temp1,temp2,temp3)
+        Log.d("debugging", people.value.toString())
+
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_people_list, parent, false)
+        return PersonHolder(view)
     }
 
     override fun onBindViewHolder(holder: PeopleListAdapter.PersonHolder, position: Int) {
         people.value!!.get(position).let { item ->
             with(holder) {
-                name.text = item.name
-                content.text = item.content
-                Log.e("TextSet", name.text.toString() + content.text.toString())
+                textViewName.text = item.name
+                textViewContent.text = item.content
+                textViewTime.text = item.timestamp.toString()
+
+                Log.e("TextSet", textViewName.text.toString() + textViewContent.text.toString())
             }
         }
     }
 
     override fun getItemCount(): Int {
-       // Log.e("datasize", "" + people.value!!.size)
+       Log.e("datasize", "" + people.value!!.size)
         return people.value!!.size
+    }
+
+    fun setList(input:MutableLiveData<List<Person>>){
+        people.value = input.value
+        notifyDataSetChanged()
+
+
     }
 }
--- presentation/src/main/java/com/oyoyoy/karma/peopleList/PeopleListViewModel.kt
+++ presentation/src/main/java/com/oyoyoy/karma/peopleList/PeopleListViewModel.kt
@@ -1,14 +1,43 @@
 package com.oyoyoy.karma.peopleList
 
+
 import androidx.lifecycle.MutableLiveData
-import com.oyoyoy.karma.base.BaseViewModel
+
 import com.oyoyoy.karma.peopleList.test.Person
 
-class PeopleListViewModel : BaseViewModel() {
-    var peopleList : MutableLiveData<List<Person>> = MutableLiveData()
+class PeopleListViewModel  {
+    var list = mutableListOf<Person>()
+    val peopleList = MutableLiveData<List<Person>>()
 
-    override fun onCreate() {
-        super.onCreate()
+
+
+    var personAdapter: PeopleListAdapter = PeopleListAdapter(peopleList)
+    init {
+        ///testCode
+        var temp1 = Person("one","test1",11111)
+        var temp2 = Person("two","test2",22222)
+        var temp3 = Person("three","test3",33333)
+
+        //peopleList.value = listOf(temp1,temp2,temp3)
+        addItem(temp1)
+        addItem(temp2)
+        addItem(temp3)
+        personAdapter.setList(peopleList)
+        //Log.d("debugging",peopleList.toString())
+
     }
+
+
+
+    fun addItem(item: Person) {
+        list.add(item)
+        peopleList.value = list
+    }
+
+    fun removeItem(item: Person) {
+        list.remove(item)
+        peopleList.value = list
+    }
+
 
 }
