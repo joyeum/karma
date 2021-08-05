9f22e7e3940a69f83f10327bb48e832ab0ff6b24
joyeum-smoothy <doyoyoye@gmail.com>
2021-08-06 01:14:16 +0900

    0806 깃 오류 수정

---
+++ domain/src/main/java/com/oyoyoy/domain/entity/Person.kt
@@ -0,0 +1,14 @@
+package com.oyoyoy.domain.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "people_table")
+data class Person(
+    @PrimaryKey val id : Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "content") val content: String,
+    @ColumnInfo(name = "timestamp") val timestamp: Int
+)
