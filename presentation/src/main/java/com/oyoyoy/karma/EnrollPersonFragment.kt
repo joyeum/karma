48ef36ea83ab27bdfb7230b55949d76a50c9d017
Joy <88281884+doyjoy@users.noreply.github.com>
2021-08-19 00:46:20 +0900

    210819 Fragment Result 공부
    아직 작업 x

--- presentation/src/main/java/com/oyoyoy/karma/EnrollPersonFragment.kt
+++ presentation/src/main/java/com/oyoyoy/karma/EnrollPersonFragment.kt
@@ -5,32 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import com.oyoyoy.karma.base.BaseFragment
 import com.oyoyoy.karma.databinding.FragmentEnrollPersonBinding
+import java.util.Observer
 
 class EnrollPersonFragment: BaseFragment<FragmentEnrollPersonBinding>() {
     override val layoutId: Int
         get() = R.layout.fragment_enroll_person
 
-    private lateinit var editPersonName: EditText
-    private lateinit var editPersonContent: EditText
+    //private lateinit var editPersonName: EditText
+    //private lateinit var editPersonContent: EditText
+    lateinit var viewModel: EnrollPersonViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-
-
-
-
-        return super.onCreateView(inflater, container, savedInstanceState)
+         super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        editPersonName = view.findViewById(R.id.editPersonName)
-        editPersonContent = view.findViewById(R.id.editPersonContent)
 
+        viewModel = EnrollPersonViewModel()
+        //editPersonName = view.findViewById(R.id.editPersonName)
+        //editPersonContent = view.findViewById(R.id.editPersonContent)
+        viewModel.enrollClick.observe(viewLifecycleOwner, Observer {
+            //navController.navigate(R.id.action_test1Fragment_to_randomFragment)
+            setFragmentResult("requestKey", bundleOf("key" to viewModel.))
+
+        })
 
     }
+
+
+
 
 
 }
